eb296f0a6912e67505a03cb42d37bbe60d751431
wayungi <fwayungi@gmail.com>
2023-01-30 22:48:01 +0300

    ADD: Fish banner iamge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import FishScreen from './components/partials/FishScreen';
 import Navbar from './components/partials/Navbar';
 
 function App() {
   return (
     <main className="App">
       <Navbar />
+      <FishScreen />
     </main>
   );
 }
---
+++ src/components/partials/FishScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Fish from '../../resources/images/fish.jpg';
+
+const FishScreen = () => {
+  return (
+    <section className='banner'>
+      <img src={Fish} alt="fish"/>
+    </section>
+  )
+};
+
+export default FishScreen;
