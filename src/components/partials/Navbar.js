943cb2aaf6212cd452db568ae6545e6b6ed2e2bc
wayungi <fwayungi@gmail.com>
2023-01-30 21:45:24 +0300

    ADD: Navbar

--- src/components/partials/Navbar.js
+++ src/components/partials/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { CiSearch } from 'react-icons/ci'
 
 const Navbar = () => {
   return (
     <nav>
-
+      <ul>
+        <li><GiHamburgerMenu /></li>
+        <li>
+          <form>
+            <input type="text" />
+            <CiSearch />
+          </form>
+        </li>
+        <li>Home</li>
+      </ul>
     </nav>
   )
 }
