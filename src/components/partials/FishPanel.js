3a4d95c5c954cddd60e03039b3f1f266a5f3cdcd
wayungi <fwayungi@gmail.com>
2023-01-31 23:18:58 +0300

    ADD: icon for missing image

--- src/components/partials/FishPanel.js
+++ src/components/partials/FishPanel.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
+import missing from '../../images/missing.jpg';
 
-const FishPanel = ({fish}) => {
+
+const FishPanel = ({ fish }) => {
+    //console.log(fish);
+    const images = fish['Image Gallery'];
+  
   return (
     <article>
-        <h2>species name</h2>
+        <h2>{fish['Species Name']}</h2>
         <div>
             <img src='' alt="fish images" />
         </div>
-        <p>Scientific name: <span>Fish scientifi name</span></p>
-        <p>NOAA Fisherires region: <span>fish region</span></p>
+        <p>Scientific name: <span>{fish['Scientific Name']}</span></p>
+        <p>NOAA Fisherires region: <span>{fish['NOAA Fisheries Region']}</span></p>
     </article>
   )
 }
