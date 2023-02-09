ee286b4b34dc5ee191d4ea00c87cbc0bceca133a
wayungi <fwayungi@gmail.com>
2023-02-09 23:42:07 +0300

    ADD: fish logo

--- src/components/partials/Navbar.js
+++ src/components/partials/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { CiSearch } from 'react-icons/ci';
+import logo from '../../images/logo.png';
 
 const Navbar = () => (
   <nav>
     <ul>
       <li><GiHamburgerMenu /></li>
+      <li><img src={logo} alt="fish logo" /></li>
       <li>
         <form>
           <input type="text" />
