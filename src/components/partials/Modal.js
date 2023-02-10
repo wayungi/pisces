3b19b1803e348a0d6efbf479487d8a84c701ebf8
wayungi <fwayungi@gmail.com>
2023-02-10 19:51:34 +0300

    ADD: state to hide / display modal

--- src/components/partials/Modal.js
+++ src/components/partials/Modal.js
@@ -3,13 +3,13 @@ import { NavLink } from 'react-router-dom';
 import { AiOutlineClose } from 'react-icons/ai';
 import HomePage from '../pages/HomePage';
 
-const Model = () => (
+const Modal = () => (
   <section className="flex flex-col p-3 text-white justify-start bg-gray-700 absolute min-h-screen w-full opacity-90 ">
     <div className="py-7">
-      <AiOutlineClose className="text-3xl cursor-pointer" />
+      <AiOutlineClose className="text-3xl cursor-pointer"/>
     </div>
     <NavLink to="/" element={<HomePage />} className="text-2xl">Home</NavLink>
   </section>
 );
 
-export default Model;
+export default Modal;
--- src/components/partials/Navbar.js
+++ src/components/partials/Navbar.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { CiSearch } from 'react-icons/ci';
 import logo from '../../images/logo.png';
-import Model from './Model';
+import Modal from './Modal';
 
-const Navbar = () => (
-  <nav>
-    <Model />
-    <ul className="flex flex-row items-center">
-      <li className="mx-3 cursor-pointer"><GiHamburgerMenu /></li>
-      <li className="grow">
-        <form className="flex flex-row border-solid border-2 border-gray-400 rounded-sm items-center pr-1">
-          <input type="text" className="py-2 px-2 h-6 box-border focus:outline-none grow" />
-          <CiSearch className="" />
-        </form>
-      </li>
-      <li><img src={logo} alt="fish logo" className="h-10 px-1 mr-1" /></li>
-      <li className="hidden">Home</li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <nav>
+      {showModal && <Modal />}
+      <ul className="flex flex-row items-center">
+        <li className="mx-3 cursor-pointer" onclick={}><GiHamburgerMenu /></li>
+        <li className="grow">
+          <form className="flex flex-row border-solid border-2 border-gray-400 rounded-sm items-center pr-1">
+            <input type="text" className="py-2 px-2 h-6 box-border focus:outline-none grow" />
+            <CiSearch className="" />
+          </form>
+        </li>
+        <li><img src={logo} alt="fish logo" className="h-10 px-1 mr-1" /></li>
+        <li className="hidden">Home</li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
